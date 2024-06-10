e746304ba1912fea7a8a0a02ac3393124f4dc78e
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-06-10 10:25:58 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Andrea", 22);
@@ -12,10 +10,10 @@ public class Main {
         Student student4 = new Student ("Francesca", 23);
         Student student5 = new Student ("Paola", 25);
         Student student6 = new Student ("Alberto", 26);
-
+// Vado a crearmi un arraylist in cui inserirò gli studenti
         List<Student> list1 = Arrays.asList(student1,student2);
         System.out.println("List is: " + list1);
-
+//aggiorno la lista, creandendo un nuovo arraylist contenente il nome di altri studenti
         List<Student> list2 = new ArrayList<>();
         list2.add(student1);
         list2.add(student2);
